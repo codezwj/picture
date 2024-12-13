f8e8cf7ee2d531161ffc8beb48c7da99209ef1f8
nxbhx <491852417@qq.com>
2024-12-14 01:40:39 +0800

    项目初始化

---
+++ src/main/java/com/juicew/juicepicbackend/JuicePicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.juicew.juicepicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuicePicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuicePicBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juicew/juicepicbackend/JuicePicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juicew.juicepicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuicePicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
