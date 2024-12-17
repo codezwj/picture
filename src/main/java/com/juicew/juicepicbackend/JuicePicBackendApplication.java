c27d3d7c516e711022da68f0f2978eddccfb39ee
nxbhx <491852417@qq.com>
2024-12-18 01:21:58 +0800

    添加通用模板

--- src/main/java/com/juicew/juicepicbackend/JuicePicBackendApplication.java
+++ src/main/java/com/juicew/juicepicbackend/JuicePicBackendApplication.java
@@ -1,9 +1,13 @@
 package com.juicew.juicepicbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.juicew.juicepicbackend.mapper")
 public class JuicePicBackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/juicew/juicepicbackend/common/ResultUtils.java
@@ -0,0 +1,54 @@
+package com.juicew.juicepicbackend.common;
+
+import com.juicew.juicepicbackend.exception.ErrorCode;
+
+/**
+ * 响应工具类
+ *
+ */
+public class ResultUtils {
+
+    /**
+     * 成功
+     *
+     * @param data  数据
+     * @return  响应
+     * @param <T>  数据类型
+     */
+    public static <T> BaseResponse<T> success(T data){
+        return new BaseResponse<>(0,data,"ok");
+    }
+
+    /**
+     * 失败
+     *
+     * @param code  错误码
+     * @param message  错误信息
+     * @return  响应
+     */
+    public static BaseResponse<?> error(int code, String message){
+        return new BaseResponse<>(code, null, message);
+    }
+
+    /**
+     * 失败
+     *
+     * @param errorCode  错误码
+     * @return  响应
+     */
+    public static BaseResponse<?> error(ErrorCode errorCode){
+        return new BaseResponse<>(errorCode);
+    }
+
+    /**
+     * 失败
+     *
+     * @param errorCode 错误码
+     * @param message 错误信息
+     * @return  响应
+     */
+    public static BaseResponse<?> error(ErrorCode errorCode, String message){
+        return new BaseResponse<>(errorCode.getCode(), null, message);
+    }
+
+}
