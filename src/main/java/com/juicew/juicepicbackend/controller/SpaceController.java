8ae9ce25d91e197bf3c586724656a819fde2fa0b
nxbhx <491852417@qq.com>
2025-01-11 18:48:58 +0800

    图库数据分析功能

--- src/main/java/com/juicew/juicepicbackend/controller/SpaceController.java
+++ src/main/java/com/juicew/juicepicbackend/controller/SpaceController.java
@@ -74,9 +74,10 @@ public class SpaceController {
         Space oldSpace = spaceService.getById(id);
         ThrowUtils.throwIf(oldSpace == null, ErrorCode.NOT_FOUND_ERROR);
         //仅本人或管理员可删除
-        if(!oldSpace.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)){
-            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
-        }
+//        if(!oldSpace.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)){
+//            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+//        }
+        spaceService.checkSpaceAuth(loginUser,oldSpace);
         //操作数据库
         boolean result = spaceService.removeById(id);
         ThrowUtils.throwIf(!result,ErrorCode.OPERATION_ERROR);
@@ -191,9 +192,10 @@ public class SpaceController {
         Space oldSpace = spaceService.getById(id);
         ThrowUtils.throwIf(oldSpace == null, ErrorCode.NOT_FOUND_ERROR);
         // 仅本人或管理员可编辑
-        if (!oldSpace.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
-            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
-        }
+//        if (!oldSpace.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+//            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+//        }
+        spaceService.checkSpaceAuth(loginUser,oldSpace);
         // 操作数据库
         boolean result = spaceService.updateById(space);
         ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
